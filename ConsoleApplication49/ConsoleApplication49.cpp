101815a9f3f784f388055f9bc2e7e90d4a0678d7
bpv834 <81566093+bpv834@users.noreply.github.com>
2021-05-16 16:02:46 +0900

    초기 커밋.

---
+++ ConsoleApplication49/ConsoleApplication49.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main() {
+	char str1[100] = "";
+	int sz = sizeof(str1) / sizeof(str1[0]);
+	int i = 0;
+	int count = 0;
+	char* p = str1;
+	printf("문자열?");
+	gets_s(str1, sizeof(str1));
+
+	while (p[0] != '\0') {
+		if (isalpha(p[0])) {
+			count++;
+		}
+		p++;
+	}
+	printf("영문자의 개수:%d", count);
+}
